abffde932434e32e15defff792cf8ea0ffc5c4ed
Leandro Teixeira da silva <ext_leandro.teixeira@cargobr.com>
2024-08-31 20:01:16 -0300

    Ajustes em classes model

--- LanchoneteApi/Models/Pedido.cs
+++ LanchoneteApi/Models/Pedido.cs
@@ -1,12 +1,16 @@
 public class Pedido
 {
+    public Pedido()
+    {
+        Produtos = new List<Produto>();
+    }
+
     public int Id { get; set; }
-    public int DepartamentoId { get; set; }
-    public Departamento Departamento { get; set; }
+    public int DepartamentoId { get; set; }    
     public DateTime DataCriacao { get; set; }
     public string? Periodo { get; set; }
     public string? NomeCliente { get; set; }
-    public string? StatusPedido { get; set; } // Liberado, Pendente, Cancelado
+    public string? StatusPedido { get; set; } // Pendente, Liberado, Entregue, Cancelado
     public bool Pago { get; set; }
     public decimal ValorTotal { get; set; }
     public string? TipoPagamento { get; set; } // Dinheiro, Pix, Cartão, Pagar Depois
